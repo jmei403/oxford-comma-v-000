07bbe59720a4569c1e8f0096c5d62128abaf665a
Jason Mei <jason.mei403@gmail.com>
2018-05-25 02:13:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,4 @@
 def oxford_comma(array)
-  # new_string = ""
-  # (0..array.length - 1).each do |i|
-  #   if array.size < 2
-  #     new_string << array[i]
-  #   elsif array.size == 2 && i == array.length - 1
-  #     new_string << " and " + array[i]
-  #   elsif array.size > 2 && i == array.length - 1
-  #     new_string << "and " + array[i]
-  #   else
-  #     new_string << array[i] + ", "
-  #   end
-  # end
-  # new_string
-
   new_string = ""
   (0..array.length - 1).each do |i|
     if array.size == 1
